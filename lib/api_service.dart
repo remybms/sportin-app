6d20b1089d411f248f6253928e654095603532c6
JellyPh1sh <sacha.sorgiati@gmail.com>
2025-03-31 15:41:42 +0200

    :sparkles: | Add create program link api

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sportin/models/program/program_model.dart';
 import 'package:sportin/models/user_login_model.dart';
 import 'models/user_model.dart';
 
@@ -30,6 +32,44 @@ class ApiService {
     if (response.statusCode == 200) {
       return UserLoginResponse.fromJson(json.decode(response.body));
     } else {
+      throw Exception(json.decode(response.body)['message']);
+    }
+  }
+
+  Future<ProgramResponse> getPrograms() async {
+    final response = await http.get(
+      Uri.parse(baseUrl + '/programs/user'),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      return ProgramResponse.fromJson(json.decode(response.body));
+    } else {
+      throw Exception(json.decode(response.body)['message']);
+    }
+  }
+
+  Future<ProgramResponse> createProgram(Program program) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('api_token');
+
+    if (token == null) {
+      throw Exception('Token not found. Please log in again.');
+    }
+
+    final response = await http.post(
+      Uri.parse(baseUrl + '/programs/'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      },
+      body: json.encode(program.toJson()), // Add the program data here
+    );
+
+    if (response.statusCode == 201) {
+      return ProgramResponse.fromJson(json.decode(response.body));
+    } else {
+      print('Error: ${response.statusCode}');
       throw Exception(json.decode(response.body)['message']);
     }
   }
